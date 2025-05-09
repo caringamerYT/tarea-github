f332066eb739b95a9862111b9ad31db9c46fc59a
karim <carin.gatica@gmail.com>
2025-05-09 15:13:16 -0400

    cambie el algoritmo, porque al parecer no puede ser por hecho ChatGPT

--- algoritmo.py
+++ algoritmo.py
@@ -1,10 +1,1 @@
-n = 1203
-
-for i in range(0, len(n)):
-    num = n[i]
-    num_str = str(num)
-
-    digits = []
-    for d in num_str:
-        digits.append(int(d))
-        print (d, digits)
+print("Hello World")
